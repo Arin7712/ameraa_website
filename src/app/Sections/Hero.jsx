93e73317e7d6b8858e7cd4a976794ac7e3d25c8f
Yaser Siddiquee <yaser.siddiquee@gmail.com>
2025-03-18 03:19:50 +0530

    hero image adjusted

--- src/app/Sections/Hero.jsx
+++ src/app/Sections/Hero.jsx
@@ -2,7 +2,7 @@ import YButton from "../Components/YButton";
 
 export default function Hero() {
     return (
-      <div className="relative mx-auto px-4 sm:px-6 lg:px-8 relative z-10 py-20 max-sm:pt-10 pb-40 max-sm:pb-10 bg-[url('/money21.png')] max-sm:bg-[url('/money22.png')] bg-cover max-sm:bg-[center_top_-100px] bg-[center_top_-240px] bg-no-repeat">
+      <div className="relative mx-auto px-4 sm:px-6 lg:px-8 relative z-10 py-20 max-sm:pt-10 pb-40 max-sm:pb-10 bg-[url('/money21.png')] max-sm:bg-[url('/money22.png')] bg-cover max-sm:bg-[center_bottom_0px] bg-[center_top_-240px] bg-no-repeat">
         <div className="mx-auto max-w-7xl">
           <p className="text-white font-bold max-sm:text-3xl text-6xl drop-shadow-[0_6px_6px_rgba(0,0,0,0.5)]">Securing <span className="text-[#FFFB00]">High Value</span> Project Funding</p>
           <p className="text-white mt-8 mb-14 max-sm:text-sm max-w-[700px]">Get ₹10Cr+ project funding from banks and investors. We arrange LCs & BGs for ₹10Cr+ import-export deals and ₹1Cr+ without property via international banks. Based in Lucknow, serving PAN India</p>
